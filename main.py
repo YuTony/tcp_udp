47893acca4e4c87258d86d49d5a39a31c015d3ba
YuTony <versities@gmail.com>
2022-11-02 00:34:12 +0300

    fix: tcp server now uses select

--- main.py
+++ main.py
@@ -1,40 +1,68 @@
 import socket
 import sys
-import threading
-
-
-def msg_loop(conn, addr):
-    with conn:
-        print('Connected ', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(data.decode('UTF-8'))
-            conn.sendall(data)
-    print("Disconnected ", addr)
+import select
 
 
 def tcp_server(addr: str, port: int):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket() as s:
+        s.setblocking(False)
         s.bind((addr, port))
-        s.listen(1)
+        s.listen(5)
+
+        inputs = {s}
+        output = set()
+        msgs = {}
+
         while True:
-            conn, addr = s.accept()
-            thread = threading.Thread(target=msg_loop, args=(conn, addr))
-            thread.start()
+            readers, writers, excepts = select.select(inputs, output, inputs)
+            for conn in readers:
+                if conn == s:
+                    conn, client_addr = s.accept()
+                    conn.setblocking(False)
+                    inputs.add(conn)
+                    msgs[conn] = []
+                    print('Connected ', client_addr)
+                else:
+                    data = conn.recv(1024)
+                    if data:
+                        msg = data
+                        print(f"Msg from {conn.getpeername()}: {msg.decode('UTF-8')}")
+                        msgs[conn].append(msg)
+                        output.add(conn)
+                    else:
+                        print("Disconnected ", conn.getpeername())
+                        conn.close()
+                        inputs.remove(conn)
+                        if conn in output:
+                            output.remove(conn)
+                        msgs.pop(conn, None)
+
+            for conn in writers:
+                for msg in msgs[conn]:
+                    conn.sendall(msg)
+                msgs[conn] = []
+                output.remove(conn)
+
+            for conn in excepts:
+                inputs.remove(conn)
+                if conn in output:
+                    output.remove(conn)
+                del msgs[conn]
+                conn.close()
+                print("Disconnected", conn.getpeername())
 
 
 def tcp_client(addr: str, port: int):
+    print("Chat with yourself")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((addr, port))
         while True:
-            msg = input()
+            msg = input("> ")
+            if not msg:
+                break
             s.sendall(msg.encode('UTF-8'))
             data = s.recv(1024)
-            print('Received', repr(data))
-            if data == b'\n':
-                break
+            print('Received', repr(data.decode("UTF-8")))
 
 
 def udp_server(addr: str, port: int):
